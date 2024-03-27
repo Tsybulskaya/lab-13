c1106cfef8e8c943248fc8719627e5e79e165d61
Tsybulskaya <cibulskaylydmila@gmail.com>
2024-03-27 14:00:55 +0300

    9

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.printf("Hello aad wel6rcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 21; i <= 66; i++) {
+        for (it i = 21; i <= 66; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
